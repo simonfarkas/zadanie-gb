c458e5b6d9d5ed8ebcf125ed7fce37b939f0128b
Simon Farkas <simonfarkasdev@gmail.com>
2025-05-21 08:44:47 +0200

    fix(fonts): Remove unused fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Layout } from "@/layouts/Layout";
-
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
 	title: "Case Study GymBeam",
@@ -25,9 +14,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
+			<body>
 				<Layout>{children}</Layout>
 			</body>
 		</html>
